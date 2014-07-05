d3d3b45b8a9bb95b946dc59048ba9a1be2ef21e6
cornbreadninja <kpanos@gmail.com>
2014-07-05 10:10:12 -0500

    Update chair_alarm.ino
    Updated code and comments to final working version.

--- chair_alarm.ino
+++ chair_alarm.ino
@@ -2,7 +2,7 @@
 
 
 int flexiForcePin = 1;    // analog pin 0
-int motorPin = 3;         // LED for status
+int motorPin = 0;         // tiny vibration motor for feedback
 int curCounter = 0;       // current counter - goes up by 1 every second while sitting
 
 
@@ -14,38 +14,34 @@ void setup(){
 
 void loop(){
   int flexiForceReading = analogRead(flexiForcePin); 
-  //Serial.println(flexiForceReading);                                  // use this for serial monitoring where available
-  //delay(250);                                                         // slow down the output for easier reading
+  //Serial.println(flexiForceReading);                     // use this for serial monitoring where available
+  //delay(250);                                            // slow down the output for easier reading
 
   // **********************************************************
   // Check to see how long I've been sitting. Ideally, I will 
   // get up every 20 minutes and stretch.
   
-  if (curCounter >= 100){
-      digitalWrite(LEDpin, HIGH);
+  if (curCounter >= 1200){                // 20 minutes = 1200
+      digitalWrite(motorPin, HIGH);       // light the LED after x time
+      digitalWrite(1, HIGH);              // light board LED for testing
   }
   
   
   // **********************************************************
   // If my weight has been in the chair for x amount of time, 
-  // vibrate a tiny motor to alert me. 
+  // vibrate the tiny motor to alert me. 
   //
   // When my weight is no longer detected, reset the counter.
   
-  if (flexiForceReading > 10){             // while I'm sitting
-    //if (time > 1000) {                   // if x time has passed  
-      curCounter++;                        // increment sitting counter
-      digitalWrite(motorPin, HIGH);        // buzz the motor
-      //Serial.println("sitting");    
-      //Serial.println("Time: ");
-      //Serial.println(time);
+   if (flexiForceReading > 10){         // while I'm sitting
+      curCounter++;                     // increment sitting counter
     }
-    
-    else if (flexiForceReading < 10) {     // if I am standing
-      digitalWrite(motorPin, LOW);         // stop the motor
-      curCounter = 0;                      // reset counter to 0
-      //Serial.println("standing");
+    else if (flexiForceReading < 10) {  // if I am standing
+      digitalWrite(motorPin, LOW);      // don't buzz the motor
+      curCounter = 0;                   // reset the counter to 0
+      digitalWrite(1, LOW);             // turn off trinket's board LED
     }
-    
+ 
   delay(1000);
 }
+
